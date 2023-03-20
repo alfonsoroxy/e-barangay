095602f18898f5015b9e8825d5317e0776fb8cab
alfonsoroxy <alfonsoroxy031.pci@gmail.com>
2023-03-20 22:13:07 +0800

    Configure plugins

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets/dist/css', 'public/assets/dist/js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        // laravel({
-        //     input: ['resources/css/app.css', 'resources/js/app.js'],
-        //     refresh: true,
-        // }),
     ],
 });
