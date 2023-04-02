1957000263c11ba8c5b61f50c7f10889a0ee7e2f
alfonsoroxy <alfonsoroxy031.pci@gmail.com>
2023-04-02 15:38:51 +0800

    Fixed image public path

--- resources/views/livewire/admin/admin-barangay-permit-component.blade.php
+++ resources/views/livewire/admin/admin-barangay-permit-component.blade.php
@@ -171,10 +171,10 @@
                                                 </div>
                                             </td>
                                             <td class="d-flex justify-content-center">
-                                                <a href="{{ secure_asset('assets/dist/img/barangay-permits/'.$barangay_permit->barangayPermitImage) }}" 
+                                                <a href="{{ asset('assets/dist/img/barangay-permits/') }}/{{$barangay_permit->barangayPermitImage}}" 
                                                     target="_blank" rel="noopener noreferrer">
                                                     <img alt="Valid Barangay Permit" class="profile-user-img img-fluid img-square"
-                                                    src="{{ secure_asset('assets/dist/img/barangay-permits/'.$barangay_permit->barangayPermitImage) }}" />
+                                                    src="{{ asset('assets/dist/img/barangay-permits') }}/{{$barangay_permit->barangayPermitImage}}" />
                                                 </a>
                                             </td>
                                         </tr>
