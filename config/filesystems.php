f4ae5a54c0ec444e94aa4ba3126928e57214d8f0
alfonsoroxy <alfonsoroxy031.pci@gmail.com>
2023-04-13 19:10:20 +0800

    Test

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,6 @@ return [
 
         'local' => [
             'driver' => 'local',
-            // 'root' => storage_path('app/public'),
             'root' => public_path() . '/assets/dist/img/',
         ],
 
